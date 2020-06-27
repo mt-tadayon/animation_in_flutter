f88c7fb9603ece864876969a53d03ed707022f7f
Mantab Tadayon <mt.tadayon@gmail.com>
2020-06-27 16:44:49 +0200

    Add new animation for AnimationWidget

--- lib/animated_builder_animation.dart
+++ lib/animated_builder_animation.dart
@@ -1,14 +1,61 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/wave_path.dart';
+
 class AnimatedBuilderAnimation extends StatefulWidget {
   @override
-  _AnimatedBuilderAnimationState createState() => _AnimatedBuilderAnimationState();
+  _AnimatedBuilderAnimationState createState() =>
+      _AnimatedBuilderAnimationState();
 }
 
-class _AnimatedBuilderAnimationState extends State<AnimatedBuilderAnimation> {
+class _AnimatedBuilderAnimationState extends State<AnimatedBuilderAnimation>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+  Animation _animation;
+
+  @override
+  void initState() {
+    _controller =
+        AnimationController(duration: Duration(seconds: 10), vsync: this)..repeat();
+    _animation = Tween<double>(begin: -500, end: 0).animate(_controller);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        title: Text('AnimatedBuilder'),
+      ),
+      body: AnimatedBuilder(
+        animation: _animation,
+        child: WavePath(),
+        builder: (_, waveChild) {
+          return Stack(
+            children: [
+              Positioned(
+                bottom: 0,
+                right: _animation.value,
+                child: waveChild,
+              ),
+              Positioned(
+                bottom: 0,
+                left: _animation.value,
+                child: waveChild,
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
+
+
---
+++ lib/widgets/wave_path.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class WavePath extends StatelessWidget {
+  const WavePath({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipPath(
+      clipper: WaveClipper(),
+      child: Opacity(
+        opacity: 0.5,
+        child: Container(
+          color: Colors.blue,
+          width: 1000,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
+
+class WaveClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = new Path();
+
+    path.lineTo(0.0, 40.0);
+    path.lineTo(0.0, size.height);
+    path.lineTo(size.width, size.height);
+    path.lineTo(size.width, 40.0);
+
+    for (int i = 0; i < 10; i++) {
+      if (i % 2 == 0) {
+        path.quadraticBezierTo(
+            size.width - (size.width / 16) - (i * size.width / 8),
+            0.0,
+            size.width - ((i + 1) * size.width / 8),
+            size.height - 160);
+      } else {
+        path.quadraticBezierTo(
+            size.width - (size.width / 16) - (i * size.width / 8),
+            size.height - 120,
+            size.width - ((i + 1) * size.width / 8),
+            size.height - 160);
+      }
+    }
+
+    path.lineTo(0.0, 40.0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
